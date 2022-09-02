76ab1f860737a2ac2bd2eeba339d153a32963e1a
Richard Servello <rservello@eastoflapost.com>
2022-09-02 11:24:52 -0700

    fixed predict_sr which was the old version for some reason

--- predict_sr.py
+++ predict_sr.py
@@ -1,9 +1,11 @@
 # Prediction interface for Cog ⚙️
 # https://github.com/replicate/cog/blob/main/docs/python.md
 
-from cog import BasePredictor, Input, Path, File
+# from cog import BasePredictor, Input, Path, File
 import argparse, os, sys, glob
-import torch, torchvision
+# import torch, torchvision
+import torchvision
+import torch.cuda
 import numpy as np
 from omegaconf import OmegaConf
 from PIL import Image
@@ -22,122 +24,122 @@ from ldm.models.diffusion.plms import PLMSSampler
 
 sys.path.append("latent-diffusion")
 
-ckpt = "/root/.cache/ldm/sr/last.ckpt"
+ckpt = "models/ldm/bsr_sr/model.ckpt"
 
 sizes = [128, 256, 384, 448, 512]
 
 
-class Predictor(BasePredictor):
-    def setup(self):
-        subprocess.call(["pip", "install", "-e", "."])
-        global config, model, global_step, device
-        device = torch.device("cuda")
-        config = OmegaConf.load("/src/configs/latent-diffusion/superres.yaml")
-        print(f"Loading model from {ckpt}")
-        pl_sd = torch.load(ckpt, map_location="cuda")
-        global_step = pl_sd["global_step"]
-        sd = pl_sd["state_dict"]
-        model = instantiate_from_config(config.model)
-        m, u = model.load_state_dict(sd, strict=False)
-        model.cuda()
-        model.eval()
+# class Predictor(BasePredictor):
+# def setup():
+#     # subprocess.call(["pip", "install", "-e", "."])
+#     global config, model, global_step, device
+#     device = torch.device("cuda")
+#     conf = "models/ldm/bsr_sr/config.yaml"
+#     # config = OmegaConf.load("/src/configs/latent-diffusion/superres.yaml")
+#     config = OmegaConf.load(conf)
+#     print(f"Loading model from {ckpt}")
+#     pl_sd = torch.load(ckpt, map_location="cuda")
+#     global_step = pl_sd["global_step"]
+#     sd = pl_sd["state_dict"]
+#     model = instantiate_from_config(config.model)
+#     m, u = model.load_state_dict(sd, strict=False)
+#     model.cuda()
+#     model.eval()
+#     return model
 
-    def predict(
-        self,
-        image: Path = Input(description="Image"),
-        up_f: int = Input(description="Upscale factor", default=4, choices=[2, 3, 4]),
-        steps: int = Input(description="Sampling steps", default=100),
-    ) -> Path:
-        global config, model, global_step
+def predict(BSRmodel, image, up_f, steps, outPath):
+    global config, model, global_step
+    model = BSRmodel
+    save_intermediate_vid = False
+    n_runs = 1
+    masked = False
+    guider = None
+    ckwargs = None
+    mode = "ddim"
+    ddim_use_x0_pred = False
+    temperature = 1.0
+    eta = 1.0
+    make_progrow = True
+    custom_shape = None
+    custom_steps = steps
 
-        save_intermediate_vid = False
-        n_runs = 1
-        masked = False
-        guider = None
-        ckwargs = None
-        mode = "ddim"
-        ddim_use_x0_pred = False
-        temperature = 1.0
-        eta = 1.0
-        make_progrow = True
-        custom_shape = None
-        custom_steps = steps
+    c = Image.open(image).convert("RGBA")
+    # Remove alpha channel if present
+    bg = Image.new("RGBA", c.size, (255, 255, 255))
+    c = Image.alpha_composite(bg, c).convert("RGB")
+    c = torch.unsqueeze(torchvision.transforms.ToTensor()(c), 0)
+    c_up = torchvision.transforms.functional.resize(c, size=[up_f * c.shape[2], up_f * c.shape[3]], antialias=True)
+    c_up = rearrange(c_up, "1 c h w -> 1 h w c")
+    c = rearrange(c, "1 c h w -> 1 h w c")
+    c = 2.0 * c - 1.0
+    c = c.to(torch.device("cuda"))
+    batch = {"LR_image": c, "image": c_up}
 
-        c = Image.open(image).convert("RGBA")
-        # Remove alpha channel if present
-        bg = Image.new("RGBA", c.size, (255, 255, 255))
-        c = Image.alpha_composite(bg, c).convert("RGB")
-        c = torch.unsqueeze(torchvision.transforms.ToTensor()(c), 0)
-        c_up = torchvision.transforms.functional.resize(c, size=[up_f * c.shape[2], up_f * c.shape[3]], antialias=True)
-        c_up = rearrange(c_up, "1 c h w -> 1 h w c")
-        c = rearrange(c, "1 c h w -> 1 h w c")
-        c = 2.0 * c - 1.0
-        c = c.to(torch.device("cuda"))
-        batch = {"LR_image": c, "image": c_up}
+    height, width = batch["image"].shape[1:3]
+    split_input = height >= 128 and width >= 128
 
-        height, width = batch["image"].shape[1:3]
-        split_input = height >= 128 and width >= 128
+    if split_input:
+        ks = 128
+        stride = 64
+        vqf = 4
+        model.split_input_params = {
+            "ks": (ks, ks),
+            "stride": (stride, stride),
+            "vqf": vqf,
+            "patch_distributed_vq": True,
+            "tie_braker": False,
+            "clip_max_weight": 0.5,
+            "clip_min_weight": 0.01,
+            "clip_max_tie_weight": 0.5,
+            "clip_min_tie_weight": 0.01,
+        }
+    else:
+        if hasattr(model, "split_input_params"):
+            delattr(model, "split_input_params")
 
-        if split_input:
-            ks = 128
-            stride = 64
-            vqf = 4
-            model.split_input_params = {
-                "ks": (ks, ks),
-                "stride": (stride, stride),
-                "vqf": vqf,
-                "patch_distributed_vq": True,
-                "tie_braker": False,
-                "clip_max_weight": 0.5,
-                "clip_min_weight": 0.01,
-                "clip_max_tie_weight": 0.5,
-                "clip_min_tie_weight": 0.01,
-            }
-        else:
-            if hasattr(model, "split_input_params"):
-                delattr(model, "split_input_params")
+    invert_mask = False
 
-        invert_mask = False
+    x_T = None
+    if custom_shape is not None:
+        x_T = torch.randn(1, custom_shape[1], custom_shape[2], custom_shape[3]).to(model.device)
+        x_T = repeat(x_T, "1 c h w -> b c h w", b=custom_shape[0])
+    for n in trange(n_runs, desc="Sampling"):
+        logs = make_convolutional_sample(
+            batch,
+            model,
+            mode="superresolution",
+            custom_steps=custom_steps,
+            eta=eta,
+            swap_mode=False,
+            masked=masked,
+            invert_mask=invert_mask,
+            quantize_x0=False,
+            custom_schedule=None,
+            decode_interval=10,
+            resize_enabled=False,
+            custom_shape=custom_shape,
+            temperature=temperature,
+            noise_dropout=0.0,
+            corrector=guider,
+            corrector_kwargs=ckwargs,
+            x_T=x_T,
+            save_intermediate_vid=False,
+            make_progrow=make_progrow,
+            ddim_use_x0_pred=ddim_use_x0_pred,
+        )
 
-        x_T = None
-        if custom_shape is not None:
-            x_T = torch.randn(1, custom_shape[1], custom_shape[2], custom_shape[3]).to(model.device)
-            x_T = repeat(x_T, "1 c h w -> b c h w", b=custom_shape[0])
-        for n in trange(n_runs, desc="Sampling"):
-            logs = make_convolutional_sample(
-                batch,
-                model,
-                mode="superresolution",
-                custom_steps=custom_steps,
-                eta=eta,
-                swap_mode=False,
-                masked=masked,
-                invert_mask=invert_mask,
-                quantize_x0=False,
-                custom_schedule=None,
-                decode_interval=10,
-                resize_enabled=False,
-                custom_shape=custom_shape,
-                temperature=temperature,
-                noise_dropout=0.0,
-                corrector=guider,
-                corrector_kwargs=ckwargs,
-                x_T=x_T,
-                save_intermediate_vid=False,
-                make_progrow=make_progrow,
-                ddim_use_x0_pred=ddim_use_x0_pred,
-            )
+    sample = logs["sample"]
+    sample = sample.detach().cpu()
+    sample = torch.clamp(sample, -1.0, 1.0)
+    sample = (sample + 1.0) / 2.0 * 255
+    sample = sample.numpy().astype(np.uint8)
+    sample = np.transpose(sample, (0, 2, 3, 1))
+    # outfile = tempfile.mktemp(".png")
+    img_name, ext = os.path.splitext(os.path.basename(image))
+    outfile = os.path.join(outPath, img_name+'_'+str(steps)+'_upres_latent_sr.png')
+    a = Image.fromarray(sample[0]).save(outfile)
 
-        sample = logs["sample"]
-        sample = sample.detach().cpu()
-        sample = torch.clamp(sample, -1.0, 1.0)
-        sample = (sample + 1.0) / 2.0 * 255
-        sample = sample.numpy().astype(np.uint8)
-        sample = np.transpose(sample, (0, 2, 3, 1))
-        outfile = tempfile.mktemp(".png")
-        a = Image.fromarray(sample[0]).save(outfile)
-
-        return Path(outfile)
+    return outfile
 
 
 @torch.no_grad()
